3737149489ae82b6ea2bd90121b3b45b8a5ba63e
Nick Chmela <nickchmela@gmail.com>
2022-03-13 11:00:16 -0400

    added form handling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,19 +14,27 @@ function Footer() {
               touch shortly.
             </p>
           </div>
-
-          <form action="" class="max-w-md mx-auto mt-8 mb-0 space-y-4">
+          {/* Form ----------------------------- */}
+          <form
+            action=""
+            method="POST"
+            class="max-w-md mx-auto mt-8 mb-0 space-y-4"
+          >
+            <input type="hidden" name="form-name" value="contact" />
             <div>
-              <label for="text" class="sr-only">
+              <label htmlFor="name" class="sr-only">
                 Name
               </label>
 
               <div className="group block relative text-black">
                 <span className="absolute inset-0 bg-gray-600 rounded-xl translate-x-1 translate-y-1"></span>
-                <div className="flex relative  space-x-2 h-full bg-white hover:bg-green-50 rounded-xl border-2 border-gray-600 ">
+                <div className="flex relative   h-full bg-white hover:bg-green-50 rounded-xl border-2 border-gray-600 ">
                   <input
+                    id="name"
+                    name="name"
                     type="text"
                     placeholder="Name"
+                    autoComplete="name"
                     className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm "
                   />
                 </div>
@@ -34,40 +42,45 @@ function Footer() {
             </div>
 
             <div>
-              <label for="email" class="sr-only">
+              <label htmlFor="email" class="sr-only">
                 Email
               </label>
 
               <div className="group block relative text-black">
                 <span className="absolute inset-0 bg-gray-600 rounded-xl translate-x-1 translate-y-1"></span>
-                <div className="flex relative  space-x-2 h-full bg-white hover:bg-green-50 rounded-xl border-2 border-gray-600 ">
+                <div className="flex relative   h-full bg-white hover:bg-green-50 rounded-xl border-2 border-gray-600 ">
                   <input
+                    id="email"
+                    name="email"
                     type="text"
                     placeholder="Email"
+                    autoComplete="email"
                     className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm "
                   />
                 </div>
               </div>
             </div>
             <div>
-              <label for="message" class="sr-only">
+              <label htmlFor="message" class="sr-only">
                 Message
               </label>
 
               <div className="group block relative text-black">
                 <span className="absolute inset-0 bg-gray-600 rounded-xl translate-x-1 translate-y-1"></span>
-                <div className="flex relative  space-x-2 h-full bg-white hover:bg-green-50 rounded-xl border-2 border-gray-600 ">
-                  <input
+                <div className="flex relative h-full bg-white hover:bg-green-50 rounded-xl border-2 border-gray-600 ">
+                  <textarea
+                    id="message"
+                    name="message"
                     type="text area"
-                    placeholder="Name"
-                    className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm "
+                    placeholder="Message"
+                    className="w-full p-4 pr-12 text-sm  border-gray-200 rounded-lg shadow-sm "
                   />
                 </div>
               </div>
             </div>
 
             {/* Submit button ---------------------------------------- */}
-            <div class="flex items-center justify-end">
+            <div className="flex items-center justify-end">
               <button
                 className="group block relative text-black "
                 type="submit"
@@ -80,82 +93,6 @@ function Footer() {
             </div>
           </form>
         </div>
-
-        {/* Form test */}
-
-        <form
-          action="#"
-          method="POST"
-          className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
-        >
-          <div>
-            <label
-              htmlFor="first-name"
-              className="block text-sm font-medium text-warm-gray-900"
-            >
-              First name
-            </label>
-            <div className="mt-1">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
-              />
-            </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-warm-gray-900"
-            >
-              Email
-            </label>
-            <div className="mt-1">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
-              />
-            </div>
-          </div>
-
-          <div className="sm:col-span-2">
-            <div className="flex justify-between">
-              <label
-                htmlFor="message"
-                className="block text-sm font-medium text-warm-gray-900"
-              >
-                Message
-              </label>
-              <span id="message-max" className="text-sm text-warm-gray-500">
-                Max. 500 characters
-              </span>
-            </div>
-            <div className="mt-1">
-              <textarea
-                id="message"
-                name="message"
-                rows={4}
-                className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border border-warm-gray-300 rounded-md"
-                aria-describedby="message-max"
-                defaultValue={""}
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2 sm:flex sm:justify-end">
-            <button
-              type="submit"
-              className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-teal-500 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 sm:w-auto"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
 
         {/* Additional footer content ------------------------------------------------------------*/}
         <div className="pt-12 mt-32  sm:flex sm:justify-between sm:items-center">
